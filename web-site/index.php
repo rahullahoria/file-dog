cc2a12930c46094e8784166c74eede233f9635a2
rahullahoria <rahul_lahoria@yahoo.com>
2016-06-22 20:10:07 +0530

    file push 1

--- web-site/index.php
+++ web-site/index.php
@@ -7,5 +7,29 @@
  */
 
 ?>
-
+<h1>File-Dog <i>powered By Shatkon Labs</i></h1>
 <img src="logo.jpg" />
+
+<br/>
+
+<h2>Test it now</h2>
+<br/>
+1. Upload a file
+<br/>
+<form action="http://api.file-dog.shatkonlabs.com/files/rahul" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+<br/>
+2. You will get ID put it with "http://api.file-dog.shatkonlabs.com/files/rahul/YOUR-FILE-ID"
+<br/>
+you will get the file...........
+<br/>
+have fun
+
+
+
+<br/>
+
+for any issue contact "anil@shatkonlabs.com"
