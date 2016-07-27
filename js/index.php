cb167a4624c5940ec099682024d8ca51e81b8548
collap <rajnish_pawar90@yahoo.com>
2016-07-27 12:44:04 +0530

    commit

---
+++ js/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spider-ninja
+ * Date: 6/22/16
+ * Time: 3:02 PM
+ */
+
+?>
+<h1>File-Dog <i>powered By Shatkon Labs</i></h1>
+<img src="logo.jpg" />
+
+<br/>
+
+<h2>Test it now</h2>
+<br/>
+1. Upload a file
+<br/>
+<form action="http://api.file-dog.shatkonlabs.com/files/rahul" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+<br/>
+2. You will get ID put it with "http://api.file-dog.shatkonlabs.com/files/rahul/YOUR-FILE-ID"
+<br/>
+you will get the file...........
+<br/>
+have fun
+
+
+
+<br/>
+
+for any issue contact "anil@shatkonlabs.com"
